029603a2205cdd522dc541244b7385fbc4da70db
kkarrinn11 <karina_karina2005@icloud.com>
2025-04-12 20:51:46 +0300

    Доработка

--- 5_hw.py
+++ 5_hw.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 driver.get ('https://www.saucedemo.com/')
 
 
-elements = driver.find_element (By.CSS_SELECTOR, '#user-name, #password, .submit-button')
+elements = driver.find_elements (By.CSS_SELECTOR, '#user-name, #password, #login-button')
 if elements is None:
     print('Элементы не найдены')
 else:
